324d9e193278b04660099b3e9a6c6c8d0502a839
jwc20 <choij2015@gmail.com>
2023-03-06 18:26:27 -0800

    Fix naming.

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const url = "http://127.0.0.1:8000/api/events/";
+  // const url = "http://127.0.0.1:8000/api/events/";
+  const url = "http://localhost:8000/api/events/"
 
   useEffect(() => {
-    const getAllTodos = async () => {
+    const getAllEvents = async () => {
       try {
         const response = await axios.get(url);
         setIsLoading(false);
-        const allTodos = response.data;
-        console.log(allTodos);
-        setTodos(allTodos);
+        const allEvents = response.data;
+        console.log(allEvents);
+        setEvents(allEvents);
       } catch (error) {
         setIsLoading(false);
         setIsError(true);
@@ -22,7 +23,7 @@ function App() {
       }
     };
 
-    getAllTodos();
+    getAllEvents();
   }, []);
 
   if (isLoading) {
@@ -31,11 +32,13 @@ function App() {
 
   return (
     <div>
-      {todos &&
-        todos.map((item, index) => (
+      {events &&
+        events.map((item, index) => (
           <div key={item.id}>
-            <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <h1>{item.name}</h1>
+            <p>{item.location}</p>
+            <p>{item.date}</p>
+            <p>{item.event_url}</p>
           </div>
         ))}
       {isError && <div>Error fetching data.</div>}
