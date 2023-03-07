96d707b9dfcb9d635f228af6bbe30b3c1e158970
jwc20 <choij2015@gmail.com>
2023-03-06 17:07:47 -0800

    Create event model.

--- api/events/models.py
+++ api/events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    date = models.DateField()
+    event_url = models.URLField(max_length=255)
+
+    def __str__(self):
+        return self.name
