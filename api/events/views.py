791c005e895ae1fb97bead21bfbe920295c6200e
jwc20 <choij2015@gmail.com>
2023-03-06 18:15:57 -0800

    Create event views.

--- api/events/views.py
+++ api/events/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class ListEvent(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class DetailEvent(generics.RetrieveAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
