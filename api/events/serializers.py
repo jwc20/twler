bd70ac9b6f3315a18d57e192b351f58f17d0178a
jwc20 <choij2015@gmail.com>
2023-03-06 18:25:54 -0800

    Fix event serializer.

--- api/events/serializers.py
+++ api/events/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Event
 
 
-class EventSerializer(serializers.Serializer):
+class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = (
