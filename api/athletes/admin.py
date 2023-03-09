2fccc8dd11b030dc6b128de6084ad1a19ca90a29
jwc20 <choij2015@gmail.com>
2023-03-08 17:03:02 -0800

    Add models to admin panel.

--- api/athletes/admin.py
+++ api/athletes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Athlete
 
-# Register your models here.
+admin.site.register(Athlete)
--- api/eventcompetitors/admin.py
+++ api/eventcompetitors/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import EventCompetitor
 
-# Register your models here.
+admin.site.register(EventCompetitor)
