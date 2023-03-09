c61ced6c938dc649fad2ff66426327b9a1741556
jwc20 <choij2015@gmail.com>
2023-03-08 16:35:34 -0800

    Update

--- api/config/settings.py
+++ api/config/settings.py
@@ -41,8 +41,7 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        # "rest_framework.permissions.IsAuthenticated",
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
