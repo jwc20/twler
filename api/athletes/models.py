82a4bf12bd6cae1e0b7727612ef9ae3b228825a2
jwc20 <choij2015@gmail.com>
2023-03-08 16:11:39 -0800

    Add many-to-many relationship with event.

--- api/athletes/models.py
+++ api/athletes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from events.models import Event 
+# from eventcompetitors.models import EventCompetitor
 
 class Athlete(models.Model):
     first_name = models.CharField(max_length=100)
@@ -7,9 +9,11 @@ class Athlete(models.Model):
     gender = models.CharField(max_length=10)
     nation = models.CharField(max_length=100)
     birth_date = models.DateField()
-    profile_picture = models.ImageField(upload_to='athlete_profile_pics/')
-    competitions = models.ManyToManyField("Event")
-    results = models.JSONField()
+    # profile_picture = models.ImageField(upload_to='athlete_profile_pics/')
+    # results = models.JSONField()
+
+    # Relationships
+    events = models.ManyToManyField(Event, through="eventcompetitors.EventCompetitor")
 
     def __str__(self):
         return self.last_name + ", " + self.first_name
