29c3e8fb5af9f7b5fa945603a75be18993993ce4
jwc20 <choij2015@gmail.com>
2023-03-08 16:11:12 -0800

    Move to result model.

--- api/eventcompetitors/models.py
+++ api/eventcompetitors/models.py
@@ -1,27 +1,23 @@
 from django.db import models
+
+
 from events.models import Event
 from athletes.models import Athlete
 
+# from results.models import Result
+
 
 class EventCompetitor(models.Model):
-    athlete = models.OneToOneField(Athlete, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     body_weight = models.FloatField()
     weight_category = models.IntegerField()
     group = models.CharField(max_length=255)
-    snatch1 = models.FloatField()
-    snatch2 = models.FloatField()
-    snatch3 = models.FloatField()
-    snatch = models.FloatField()
-    jerk1 = models.FloatField()
-    jerk2 = models.FloatField()
-    jerk3 = models.FloatField()
-    jerk = models.FloatField()
-    total = models.FloatField()
-    rank_snatch = models.IntegerField()
-    rank_jerk = models.IntegerField()
-    rank = models.IntegerField()
-    medal = models.CharField(max_length=255)
+
+    athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    # result = models.ForeignKey(Result, on_delete=models)
 
     # def __str__(self):
     #     return
+
+    class Meta:
+        unique_together = ("event", "athlete")
