d4a3bfb24d18f948c91cfcc16f3701fc467d45d7
jwc20 <choij2015@gmail.com>
2023-03-10 16:23:29 -0800

    Fix

--- api/config/settings.py
+++ api/config/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "twler_database",
-        "USER": "twler",
+        "NAME": "postgres",
+        "USER": "postgres",
         "PASSWORD": "",
         "HOST": "db",
         "PORT": 5432,
