e771ac3ff4b005954ce139b23c85efac1f4d0294
jwc20 <choij2015@gmail.com>
2023-03-10 18:21:31 -0800

    Allow unauthenticated access to view with AllowAny

--- api/config/settings.py
+++ api/config/settings.py
@@ -41,7 +41,8 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.AllowAny",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
