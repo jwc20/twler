bf528867bd8053ddc6d99362f8dcd229045183a3
jwc20 <choij2015@gmail.com>
2023-03-10 16:10:35 -0800

    Update to use with postico.

--- api/config/settings.py
+++ api/config/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "twler_database",
+        "USER": "twler",
+        "PASSWORD": "",
         "HOST": "db",
         "PORT": 5432,
     }
