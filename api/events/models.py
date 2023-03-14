47d55df6aea3c721cb696801a9b6d0c0df3520a4
jwc20 <choij2015@gmail.com>
2023-03-13 17:43:32 -0700

    Add cid attribute for ipfs hash in event model.

--- api/events/models.py
+++ api/events/models.py
@@ -5,6 +5,7 @@ class Event(models.Model):
     location = models.CharField(max_length=255)
     date = models.DateField()
     event_url = models.URLField(max_length=255)
+    cid = models.CharField()
 
     def __str__(self):
         return self.name
