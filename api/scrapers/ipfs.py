d056bb792041e62e55194d2d204aa0db3d09b5fa
jwc20 <choij2015@gmail.com>
2023-03-16 17:31:51 -0700

    Update with generating hash function.

--- api/scrapers/ipfs.py
+++ api/scrapers/ipfs.py
@@ -1,48 +1,31 @@
 import ipfshttpclient
 
-# Share TCP connections using a context manager
-# with ipfshttpclient.connect("/dns4/ipfs0/tcp/5001") as client:
-#     hash = client.add('test.txt')['Hash']
-#     print(hash)
 
-# Share TCP connections until the client session is closed
 class IPFSClient:
+
+    # Share TCP connections until the client session is closed
     def __init__(self):
         self._client = ipfshttpclient.connect("/dns4/ipfs0/tcp/5001")
 
-    def check_results(self):
-        """
-        Check to see if the result page changed before scraping.
-        """
-        pass
+    # def check_results(self):
+    #     """
+    #     Check to see if the result page changed before scraping.
+    #     """
+    #     pass
 
-    def upload_to_ipfs(self, json_data):
-        # Need to figure out how to add json data from db.
-        # Maybe use the serializer endpoint.
-
-        # hash = self._client.add("test.txt")["Hash"]
-        print("Hello from ipfs client")
-        print(json_data)
-        __import__('pdb').set_trace()
-
-    def generate_cid_for_result(self):
+    def generate_cid_for_result(self, json_data):
         """
-        Generate hash to use as ipfs CID.
+        Add to IPFS node and generate hash to use as ipfs CID.
         """
-        pass
+        hash = self._client.add_json(json_data)
+        print(hash)
+        # __import__('pdb').set_trace()
+        return hash
 
     def close(self):  # Call this when your done
         self._client.close()
 
 
-# client = SomeObject()
-# client = ipfshttpclient.connect("/dns4/ipfs0/tcp/5001")
-# data = [1,2]
-# client.add_json(data)
-
-# data = {"Action": "Open", "Type": "PR", "Name": "IPFS", "Pubkey": 7}
-# res = client.add_json(data)
-#
 # data1 = {"Action": "Closed"}
 # print(client)
 # assert data == client.get_json(res)
@@ -52,9 +35,9 @@ class IPFSClient:
 #
 # print(client.get_json(res), client.cat(res).decode("utf-8"))
 
-# client.do_something()
 
 if __name__ == "__main__":
     client = IPFSClient()
-    client.upload_to_ipfs() 
+    # client.upload_to_ipfs()
+    client.generate_cid_for_result()
     client.close()
