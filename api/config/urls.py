634932cc89e658c473a1485a44b8e792258f13be
jwc20 <choij2015@gmail.com>
2023-03-19 15:08:19 -0700

    Created custom django endpoint for getting ipfs result data.

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .custom_views import get_ipfs_data
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/events/", include("events.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("ipfs/<str:cid>/", get_ipfs_data, name="get_ipfs_data"),
 ]
