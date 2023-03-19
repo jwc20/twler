399d9072818ec34b3d2c3e183ab6304992d36346
jwc20 <choij2015@gmail.com>
2023-03-19 16:28:54 -0700

    Update with method to fetch data from example ipfs data.

---
+++ api/config/ipfs.py
@@ -0,0 +1,26 @@
+import ipfshttpclient
+
+class IPFSClient:
+    def __init__(self):
+        self._client = ipfshttpclient.connect("/dns4/ipfs0/tcp/5001")
+
+    def generate_cid_for_result(self, json_data):
+        """
+        Add to IPFS node and generate hash to use as ipfs CID.
+        """
+        hash = self._client.add_json(json_data)
+        return hash
+    
+    def get_json_data_with_cid(self, cid): 
+        data = self._client.get_json(cid)
+        return data
+
+    def close(self):
+        self._client.close()
+
+
+if __name__ == "__main__":
+    client = IPFSClient()
+    # client.upload_to_ipfs()
+    # client.generate_cid_for_result()
+    # client.close()
--- api/config/urls.py
+++ api/config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .custom_views import get_ipfs_data
+from .views import example_get_ipfs_data
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,5 +10,6 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
-    path("ipfs/<str:cid>/", get_ipfs_data, name="get_ipfs_data"),
+    # path("results/", include("results.urls")),
+    path("api/example-ipfs-json/", example_get_ipfs_data, name="example-ipfs-json"),
 ]
