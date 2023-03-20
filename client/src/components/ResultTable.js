3feff2574a780ea924ecb57a287d7c503a0ec45e
jwc20 <choij2015@gmail.com>
2023-03-19 17:05:16 -0700

    Update with loading json data.

--- client/src/components/ResultTable.js
+++ client/src/components/ResultTable.js
@@ -1,41 +1,45 @@
-
-
-
-
-
-
 import { useState, useEffect } from "react";
 // import * as IPFS from "ipfs-core";
 import axios from "axios";
 
-
 function ResultTable({ cid }) {
   const [result, setResult] = useState([]);
-  const [eventInfo, setEventInfo] = useState([]);
+  const [eventInfo, setEventInfo] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const url = "http://localhost:8000/api/example-ipfs-json/";
 
   useEffect(() => {
-    const getEventInfo = async () => {
+    const getResult = async () => {
       try {
         const response = await axios.get(url);
-        console.log(response)
         setIsLoading(false);
-        setEventInfo(response.data);
+        // console.log(response.data);
+        // console.log(typeof response.data)
+        setResult(response.data);
       } catch (error) {
         setIsLoading(false);
         setIsError(true);
         console.log(error);
       }
     };
-    getEventInfo();
+    getResult();
   }, []);
+
+  console.log(result);
 
   return (
     <div className="result-table">
-      <p>THIS IS S ASD ASDKJASLJKDHOAIWURDIJAF RESULT TABLE</p>
+      <p>This is the result table</p>
       <p>{cid}</p>
+      {result &&
+        result.map((item, index) => (
+          <div key={item.id}>
+            <li>
+              {item.name} {item.snatch} {item.jerk} {item.rank}
+            </li>
+          </div>
+        ))}
     </div>
   );
 }
