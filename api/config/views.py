d3ec467414f9995de0b0ab4f60128d98d7f7d26b
jwc20 <choij2015@gmail.com>
2023-03-19 17:04:35 -0700

    Added method to render json from example json data.

--- api/config/views.py
+++ api/config/views.py
@@ -9,9 +9,11 @@ from ipfs import IPFSClient
 
 def example_get_ipfs_data(request):
     cid = "QmVW8NWUJvx1BgkXmhLxNFk6YiYNv9TjZZMW5cYX4CSN8H"
+
     print(cid)
     ipfs = IPFSClient()
     data = ipfs.get_json_data_with_cid(cid)
     print(type(data))
     print("IPFS data loaded")
-    return JsonResponse({"data": data})
+    return JsonResponse(data, safe=False)
+    # return JsonResponse({"data": data})
