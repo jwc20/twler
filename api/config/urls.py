f1a6bbcad5b24cd65f35d9fef3b8a7097e558d50
jwc20 <choij2015@gmail.com>
2023-03-21 17:46:52 -0700

    Update with path to results ipfs json data.

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import example_get_ipfs_data
+# from .views import example_get_ipfs_data
+from .views import get_ipfs_data
+
 
 
 urlpatterns = [
@@ -11,5 +13,6 @@ urlpatterns = [
     path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     # path("results/", include("results.urls")),
-    path("api/example-ipfs-json/", example_get_ipfs_data, name="example-ipfs-json"),
+    # path("api/example-ipfs-json/", example_get_ipfs_data, name="example-ipfs-json"),
+    path("api/ipfs/<str:cid>/", get_ipfs_data, name="ipfs-json"),
 ]
--- api/config/views.py
+++ api/config/views.py
@@ -6,14 +6,17 @@ import json
 from django.http import JsonResponse
 from ipfs import IPFSClient
 
-
-def example_get_ipfs_data(request):
-    cid = "QmVW8NWUJvx1BgkXmhLxNFk6YiYNv9TjZZMW5cYX4CSN8H"
-
-    print(cid)
+def get_ipfs_data(req, cid):
     ipfs = IPFSClient()
     data = ipfs.get_json_data_with_cid(cid)
-    print(type(data))
-    print("IPFS data loaded")
     return JsonResponse(data, safe=False)
-    # return JsonResponse({"data": data})
+
+# def example_get_ipfs_data(request):
+#     cid = "QmVW8NWUJvx1BgkXmhLxNFk6YiYNv9TjZZMW5cYX4CSN8H"
+#     print(cid)
+#     ipfs = IPFSClient()
+#     data = ipfs.get_json_data_with_cid(cid)
+#     print(type(data))
+#     print("IPFS data loaded")
+#     return JsonResponse(data, safe=False)
+#     # return JsonResponse({"data": data})
