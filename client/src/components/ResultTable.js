e971625ad903b9f0659f6a8823daaeabc692c654
jwc20 <choij2015@gmail.com>
2023-03-23 06:45:33 -0700

    Render results table with react-table.

--- client/src/components/ResultTable.js
+++ client/src/components/ResultTable.js
@@ -1,24 +1,109 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor("name", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("birthdate", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("nation", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("group", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("snatch1", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("snatch2", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("snatch3", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("snatch", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("rank_sn", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("jerk1", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("jerk2", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("jerk3", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("jerk", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("rank_cj", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("total", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("rank", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+
+];
 
 function ResultTable({ name, cid }) {
   const [result, setResult] = useState([]);
   const [eventInfo, setEventInfo] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  // const url = "http://localhost:8000/api/example-ipfs-json/";
-  // const url = "http://localhost:8000/api/ipfs/" + cid + "/";
-
   const [url, setUrl] = useState("");
+
+  const [data, setData] = useState([]);
+
+  // tanstack table
+  const rerender = useReducer(() => ({}), {})[1];
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   useEffect(() => {
     let timer;
     const getResult = async () => {
       try {
         timer = setTimeout(() => {
-          console.log(url);
+          // console.log(url);
           axios.get(url).then((response) => {
             setResult(response.data);
+            setData([...response.data])
             setIsLoading(false);
           });
           // console.log(response.data);
@@ -37,24 +122,54 @@ function ResultTable({ name, cid }) {
 
   useEffect(() => {
     const builtUrl = `http://localhost:8000/api/ipfs/${cid}`;
-    console.log(builtUrl)
+    // console.log(builtUrl);
     setUrl(builtUrl);
   }, [cid]);
 
-
   return (
     <div className="result-table">
-      <h2>{name}</h2>
-      <p>{cid}</p>
-      {result &&
-        result.map((item, index) => (
-          // The items need a key that is not the name.
-          <div key={item.name}>
-            <li>
-              {item.name} {item.snatch} {item.jerk} {item.rank}
-            </li>
-          </div>
-        ))}
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* <h2>{name}</h2> */}
+      {/* <p>{cid}</p> */}
+      {/* {result && */}
+      {/*   result.map((item, index) => ( */}
+      {/*     // The items need a key that is not the name. */}
+      {/*     <div key={item.name}> */}
+      {/*       <li> */}
+      {/*         {item.name} {item.snatch} {item.jerk} {item.rank} */}
+      {/*       </li> */}
+      {/*     </div> */}
+      {/*   ))} */}
     </div>
   );
 }
