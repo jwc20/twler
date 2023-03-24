d166e0e40caf0f48bf2f843f4f497f9ecfa268c1
jwc20 <choij2015@gmail.com>
2023-03-24 06:01:01 -0700

    Added colors for medals.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "light-gray": "#D3D3D3",
+        "gold-medal": "#FFD700",
+        "silver-medal": "#C0C0C0",
+        "bronze-medal": "#CD7F32",
       },
     },
   },
