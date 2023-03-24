3be638c8f63e837e9fd6cbf09ed3538baae932d6
jwc20 <choij2015@gmail.com>
2023-03-24 03:17:49 -0700

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,7 @@ import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage />}></Route>
